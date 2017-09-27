638283f1bfc35b406c0a76aa749627987d3ae2f3
FryOS <alexeyosp@gmail.com>
2017-09-27 15:32:32 +0300

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+var myImg = document.querySelector('img');
+myImg.onclick = function () {
+    
+    var mySrc = myImg.getAttribute('src');
+    if (mySrc === 'images/firefox_icon.png'){
+        myImg.setAttribute('src', 'images/firefox_icon1.png');
+    } else {
+        myImg.setAttribute('src', 'images/firefox_icon.png')
+    }
+    
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name');
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+           
+}
+
+if (!localStorage.getItem('name')){
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+    
+}
